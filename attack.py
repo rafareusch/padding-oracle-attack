7be1ad8a6ae8e3cd2b828971ed864ed989449b21
rafaeltoth <rafa_reusch@hotmail.com>
2022-05-05 11:01:31 -0300

    First Commit - including some reference code

---
+++ attack.py
@@ -0,0 +1,25 @@
+import urllib.request as urllib2
+import sys
+
+# doc https://robertheaton.com/2013/07/29/padding-oracle-attack/ RAFAEL
+
+
+TARGET = 'http://crypto-class.appspot.com/po?er='
+#--------------------------------------------------------------
+# padding oracle
+#--------------------------------------------------------------
+class PaddingOracle(object):
+    def query(self, q):
+        target = TARGET + urllib2.quote(q)    # Create query URL
+        req = urllib2.Request(target)         # Send HTTP request to server
+        try:
+            f = urllib2.urlopen(req)          # Wait for response
+        except (urllib2.HTTPError, e):          
+            print ("We got: %d" % e.code)       # Print response code
+            if (e.code == 404):
+                return True # good padding
+            return False # bad padding
+
+if __name__ == "__main__":
+    po = PaddingOracle()
+    po.query(sys.argv[1])       # Issue HTTP query with the given argument
